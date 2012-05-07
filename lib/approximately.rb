a0db56d2403ca9aeeac2ef6a8d4578d52f63ed88
Julik <me@julik.nl>
2012-05-08 01:27:22 +0200

    Simplify doco

--- lib/approximately.rb
+++ lib/approximately.rb
@@ -2,10 +2,10 @@ module Approximately
   VERSION = "1.0.0"
   DEFAULT_DELTA = 0.01
   
-  # This object can be used for float comparisons. When
-  # compared to another Float (or an object that responds to to_f)
-  # it will be equal to that object if the float value of the ApproximateFloat
-  # and the value being compared to are within a passed delta
+  # This object can be used for float comparisons. When it is instantiaded
+  # with a float and a delta it will respond to <=>(another_float) and
+  # will return equality if the float it's being compared to is within the
+  # delta
   class DeltaFloat < Struct.new(:float, :delta)
     include Comparable
     
@@ -16,7 +16,6 @@ module Approximately
     def <=>(another)
       d = (to_f - another.to_f).abs
       return 0 if d < delta
-      
       float <=> another.to_f
     end
   end
