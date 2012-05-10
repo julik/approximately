e5430013991a0842300998a11aae42ac677943c7
Julik <me@julik.nl>
2012-05-10 17:09:30 +0200

    Improve tests a little more

--- test/test_approximately.rb
+++ test/test_approximately.rb
@@ -1,6 +1,6 @@
 require 'helper'
 
-class TestApproximately < Test::Unit::TestCase
+class TestDeltaFloat < Test::Unit::TestCase
   include Approximately
   
   should "return an approximate object" do
@@ -11,15 +11,15 @@ class TestApproximately < Test::Unit::TestCase
     assert_in_delta 10.0, obj.to_f, 0.01
   end
   
-  should "compare to another float within delta" do
+  should "compare to another DeltaFloat within delta" do
     d = 0.1
-    assert_equal approx(1, d), approx(1, d), "Should be equal within delta"
-    assert_equal approx(1, d), approx(0.95, d), "Should be equal within delta"
-    assert_not_equal approx(1.0, d), approx(1.11, d), "Should not be equal outside delta"
+    assert_equal DeltaFloat.new(1, d), DeltaFloat.new(1, d), "Should be equal within delta"
+    assert_equal DeltaFloat.new(1, d), DeltaFloat.new(0.95, d), "Should be equal within delta"
+    assert_not_equal DeltaFloat.new(1.0, d), DeltaFloat.new(1.11, d), "Should not be equal outside delta"
   end
   
   should "compare to another float as a left-hand expression" do
-    assert_equal approx(1.0, 0.1), 1.05, "Should be equal within delta"
+    assert_equal DeltaFloat.new(1.0, 0.1), 1.05, "Should be equal within delta"
   end
   
   should "compare to another float as a right-hand expression" do
@@ -28,6 +28,14 @@ class TestApproximately < Test::Unit::TestCase
   
   should "create the object with default delta" do
     a = approx(0.013)
+    assert_kind_of Approximately::DeltaFloat, a
     assert_equal a.delta, Approximately::DEFAULT_DELTA
   end
+  
+  should "create the object with custom delta" do
+    d = 0.001
+    a = approx(0.013, d)
+    assert_equal d, a.delta
+  end
+  
 end
